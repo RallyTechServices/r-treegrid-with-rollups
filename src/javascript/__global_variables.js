a4d43f3e67c3771f88d63308715081cf14b62d4f
John Martin <curmudgeon+git@rallydev.com>
2014-06-13 09:16:36 -0700

    update headings

--- src/javascript/__global_variables.js
+++ src/javascript/__global_variables.js
@@ -1,16 +1,18 @@
  Ext.define('TSGlobals', {
     singleton: true,
+
     original_pert_header: 'Original PERT',
-    progress_by_original_header: 'Progress by Original',
-    progress_by_rollup_header: 'Progress by Rollup',
-    total_rollup_header: 'Total Rollup',
-    total_rollup_story_header: 'Total Rollup (Stories)',
-    total_rollup_defect_header: 'Total Rollup (Defects)',
-    pert_completed_header: 'PERT Completed',
-    pert_completed_story_header: 'PERT Completed (Stories)',
-    pert_completed_defect_header: 'PERT Completed (Defects)',
-    pert_remaining_header: 'Pert Remaining',
+    progress_by_original_header: '% Original',
+    progress_by_rollup_header: '% Total (Rollup)',
+    total_rollup_header: 'Total (Rollup)',
+    total_rollup_story_header: 'Story (Rollup)',
+    total_rollup_defect_header: 'Defect (Rollup)',
+    pert_completed_header: 'Total Rollup Completed',
+    pert_completed_story_header: 'Stories (Completed)',
+    pert_completed_defect_header: 'Defects (Completed)',
+    pert_remaining_header: 'Remaining (Total)',
     accepted_delta_header: 'Delta (Completed)',
     total_delta_header: 'Delta (Total)',
-    tree_header: 'Project'
+    tree_header: 'Name'
+
  });
